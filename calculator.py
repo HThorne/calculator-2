12033512d24a12e8a0d936586b7cd3a029d847a3
HThorne <hathorne@ucdavis.edu>
2023-01-25 16:59:12 -0800

    Finished operations

--- calculator.py
+++ calculator.py
@@ -5,9 +5,9 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Loop until break
-while True:ii
+while True:
     # get user input
-    user_input = input("Enter your equation: > ")
+    user_input = input("Enter your equation: ")
     # tokenize input
     tokens = user_input.split(" ")
 
@@ -15,27 +15,54 @@ while True:ii
     if "q" in tokens:
         print("Thank you, Bye")
         break 
-    
+
     # assign variable names to tokens
     operation = tokens[0]
     num1 = tokens[1]
-    
+
     if len(tokens) < 3:
         num2 = "0"
-    
+
     else:
         num2 = tokens[2]
-    
+
     if len(tokens) > 3:
         num3 = tokens[3]
-    
+
     # variable assignment for result of function
-    
+    answer = None
 
-    if ""
+
     # if statement for each operation 
-        # calls matching function for operation using other tokens
-    
+    if operation == "+":
+    # calls function that matches operation 
+    # using the float other tokens as numbers
+        answer = add(float(num1), float(num2))
 
-    
+    elif operation == "-":
+        answer = subtract(float(num1), float(num2))
+
+    elif operation == "*":
+        answer = multiply(float(num1), float(num2))
+
+    elif operation == "/":
+        answer = divide(float(num1), float(num2))
+
+    elif operation == "square":
+        answer = square(float(num1))
+
+    elif operation == "cube":
+        asnwer = cube(float(num1))
+
+    elif operation == "pow":
+        answer = power(float(num1), float(num2))
+
+    elif operation == "mod":
+        answer = mod(float(num1), float(num2))
+
+    else: 
+        answer = "Please enter an operator followed by 2 integers"
+
+    print(answer)
+
     
