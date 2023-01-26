be80685d2b140198280b2e203bc0e34343e49446
HThorne <hathorne@ucdavis.edu>
2023-01-25 16:32:25 -0800

    Get  user input, read and assign variables

--- calculator.py
+++ calculator.py
@@ -4,4 +4,38 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
-# Replace this with your code
+# Loop until break
+while True:ii
+    # get user input
+    user_input = input("Enter your equation: > ")
+    # tokenize input
+    tokens = user_input.split(" ")
+
+    # if first token is q then quit
+    if "q" in tokens:
+        print("Thank you, Bye")
+        break 
+    
+    # assign variable names to tokens
+    operation = tokens[0]
+    num1 = tokens[1]
+    
+    if len(tokens) < 3:
+        num2 = "0"
+    
+    else:
+        num2 = tokens[2]
+    
+    if len(tokens) > 3:
+        num3 = tokens[3]
+    
+    # variable assignment for result of function
+    
+
+    if ""
+    # if statement for each operation 
+        # calls matching function for operation using other tokens
+    
+
+    
+    
